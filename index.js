0981264d8a9b1d1c8c245b3ddd8d8967f947714b
AmanDevelops <amanpal2072004@gmail.com>
2025-02-10 20:02:18 +0530

    Add JavaScript functionality for login and attendance data retrieval

---
+++ index.js
@@ -0,0 +1,34 @@
+function getAllCookies() {
+    const cookies = document.cookie.split('; ');
+    const cookieMap = {};
+    cookies.forEach(cookie => {
+        const [name, value] = cookie.split('=');
+        cookieMap[name] = value;
+    });
+    return cookieMap.token;
+}
+
+
+async function getData(){
+    const url = 'https://kiet.cybervidya.net/api/attendance/course/component/student';
+    const token = getAllCookies();
+    try {
+        fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Authorization': `GlobalEducation ${token}`,
+              },
+            })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+    } catch (error) {
+        console.error(error.message);
+      }
+}
