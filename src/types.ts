cf5c9bd72f21e56fd87b295aea5f84b609b138d8
webdevgeeks <amanpal2k4@proton.me>
2025-02-21 15:31:33 +0530

    initialize Vite + React + TypeScript project with Tailwind CSS setup

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface AttendanceResponse {
+  data: {
+    fullName: string;
+    registrationNumber: string;
+    rollNumber: string;
+    sectionName: string;
+    branchShortName: string;
+    degreeName: string;
+    semesterName: string;
+    admissionBatchName: string;
+    academicSessionName: string;
+    attendanceCourseComponentInfoList: {
+      courseName: string;
+      courseCode: string;
+      attendanceCourseComponentNameInfoList: {
+        componentName: string;
+        numberOfPeriods: number;
+        numberOfPresent: number;
+        presentPercentage: number | null;
+        presentPercentageWith: string;
+      }[];
+    }[];
+  };
+  message: string;
+}
+
+export interface LoginResponse {
+  data: {
+    token: string;
+  };
+}
