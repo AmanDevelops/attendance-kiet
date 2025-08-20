3560f20df6a772c4dc16526e3df65ddb86cf6f2a
AmanDevelops <amanpal2072004@gmail.com>
2025-08-20 22:57:31 +0530

    feat: add info banner on top of page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-100">
+      <div className="overflow-hidden m-auto bg-green-100 ">
+        <div className="py-2 text-center text-sm font-medium text-green-600">
+          YOUR CREDENTIALS ARE NEVER SHARED WITH US. THEY ARE SENT DIRECTLY TO
+          CYBERVIDYA AND STORED LOCALLY.
+          <a
+            href="https://github.com/AmanDevelops/attendance-kiet"
+            className="text-blue-400"
+            target="_blank"
+          >
+            {" "}
+            VIEW SOURCE CODE
+          </a>
+        </div>
+      </div>
+
       {!attendanceData ? (
         <LoginForm setAttendanceData={setAttendanceData} />
       ) : (
